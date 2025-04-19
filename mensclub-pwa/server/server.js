a809cf69be8e865303089d5f4f6f72352e38c5c5
yeeeee._. <127736621+yetk124@users.noreply.github.com>
2025-04-19 16:00:13 +0900

    build : 모듈 삭제

--- mensclub-pwa/server/server.js
+++ mensclub-pwa/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
  
-// http://localhost:4000/ 으로 접속 시 응답메시지 출력
+// http://localhost:3000/ 으로 접속 시 응답메시지 출력
 app.get('/', (req,res) => {
     res.send({ test : "this is test!!"});
 })
