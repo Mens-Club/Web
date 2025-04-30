dd0bafdfa791d13e2b460b6ac7c9e70114aeaeca
GitSkyBlue <hiskyb@naver.com>
2025-04-30 10:38:15 +0900

    Chore: 안쓰는거 삭제

--- service/members/serializers.py
+++ service/members/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
