56d0743ff6c8691773464011ff83d1b5e71ba3ac
yeeeee._. <127736621+yetk124@users.noreply.github.com>
2025-05-01 12:11:31 +0900

    fix : 👍

--- mensclub-pwa/src/App.js
+++ mensclub-pwa/src/App.js
@@ -62,8 +62,6 @@ function AppWithNav() {
           <Route path="/edit-profile" element={<PageWrapper><EditProfilePage /></PageWrapper>} />
           <Route path="/set-password" element={<PageWrapper><SetPasswordPage /></PageWrapper>} />
           <Route path="/set-body" element={<PageWrapper><BodyInfoPage /></PageWrapper>} />
-<<<<<<< Updated upstream
-=======
           {/* <Route
             path="/reset-pw"
             element={
@@ -72,7 +70,6 @@ function AppWithNav() {
               </PageWrapper>
             }
           /> */}
->>>>>>> Stashed changes
         </Routes>
       </AnimatePresence>
 
