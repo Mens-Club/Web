650ae0cfc50a8c41a393ea02bf70951a03606119
PJH-02 <mwm3870@gmail.com>
2025-05-03 16:59:58 +0900

    Feature/Pick

--- service/Picked/models.py
+++ service/Picked/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class RecommendationTest(models.Model):
+    id = models.AutoField(primary_key=True)
+    recommendation_code = models.IntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        db_column='user_id',
+        on_delete=models.CASCADE
+    )
+    top_id = models.IntegerField()
+    bottom_id = models.IntegerField()
+    outer_id = models.IntegerField(null=True, blank=True)
+    shoes_id = models.IntegerField()
+    answer = models.TextField()
+    style = models.CharField(max_length=50)
+    total_price = models.IntegerField()
+    liked = models.BooleanField()
+    detail = models.TextField()
+    created_at = models.DateTimeField(null=False, blank=True)
+
+    class Meta:
+        db_table = 'recommendation_test'
+        managed = False
