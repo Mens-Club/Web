e927d78ffe463aa42b5cadefe8d057a6fd73750b
GitSkyBlue <hiskyb@naver.com>
2025-05-07 17:17:05 +0900

    Feat: 좋아요 토글 설정

--- service/Picked/views.py
+++ service/Picked/views.py
@@ -5,8 +5,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import RecommendationTest
 from .serializers import RecommendationTestSerializer
-
-# Swagger parameter decorators
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import RecommendationTest
+from .serializers import RecommendationTestSerializer, RecommendationIDSerializer
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
@@ -120,3 +123,28 @@ class RecommendationTestViewSet(ViewSet):
         count = int(request.query_params.get('count', 3))
         qs = self.get_base_queryset(request).order_by('?')[:count]
         return Response(self.serializer_class(qs, many=True).data)
+    
+class PickView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(request_body=RecommendationIDSerializer)
+    def post(self, request):
+        recommendation_id = request.data.get("id")
+        
+        if not recommendation_id:
+            return Response({"error": "추천 ID가 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            # recommendation_id에 해당하는 항목을 찾고 liked를 True로 설정
+            item = RecommendationTest.objects.get(id=recommendation_id)
+            item.liked = not item.liked
+            item.save()
+            return Response({"success": "추천이 저장되었습니다."}, status=status.HTTP_200_OK)
+        except RecommendationTest.DoesNotExist:
+            return Response({"error": "추천 항목을 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request):
+        user = request.user
+        picks = RecommendationTest.objects.filter(user=user, liked=True)
+        serializer = RecommendationTestSerializer(picks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
