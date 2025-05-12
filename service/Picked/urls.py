2f5771596e82506f6e8d649ee9fcc16a11b567fd
GitSkyBlue <hiskyb@naver.com>
2025-05-12 11:57:46 +0900

    Chore: picked urls 수정

--- service/Picked/urls.py
+++ service/Picked/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     path('main/random/', MainRandomAPIView.as_view(), name='main_random'),
     path('main/by-price/', MainByPriceAPIView.as_view(), name='main_by_price'),
     path('main/by-style/', MainByStyleAPIView.as_view(), name='main_by_style'),
-    path('mypage/by-time/', RecommendBookmarkByTimeAPIView.as_view(), name='bookmark_by_time'),
-    path('mypage/by-price/', RecommendBookmarkByPriceAPIView.as_view(), name='bookmark_by_price'),
-    path('mypage/by-style/', RecommendBookmarkByStyleAPIView.as_view(), name='bookmark_by_style'), # db에 style없어서 현재 수정 불가. migrations 해야함.
-    path('main_picked/by-time/', MainRecommendBookmarkByTimeAPIView.as_view(), name='main_bookmark_by_time'),
-    path('main_picked/by-price/', MainRecommendBookmarkByPriceAPIView.as_view(), name='main_bookmark_by_price'),
-    path('main_picked/by-style/', MainRecommendBookmarkByStyleAPIView.as_view(), name='main_bookmark_by_style'),
+    path('recommend_picked/by-time/', RecommendBookmarkByTimeAPIView.as_view(), name='picked_by_time'),
+    path('recommend_picked/by-price/', RecommendBookmarkByPriceAPIView.as_view(), name='picked_by_price'),
+    path('recommend_picked/by-style/', RecommendBookmarkByStyleAPIView.as_view(), name='picked_by_style'), # db에 style없어서 현재 수정 불가. migrations 해야함.
+    path('main_picked/by-time/', MainRecommendBookmarkByTimeAPIView.as_view(), name='main_picked_by_time'),
+    path('main_picked/by-price/', MainRecommendBookmarkByPriceAPIView.as_view(), name='main_picked_by_price'),
+    path('main_picked/by-style/', MainRecommendBookmarkByStyleAPIView.as_view(), name='main_picked_by_style'),
 ]
