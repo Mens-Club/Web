f9c8215a67d65f4cb29a11fa5ad94d94ba483c6d
UICHEOL_HWANG <icuchoel@gmail.com>
2025-05-16 18:55:10 +0900

    Chore: 환경변수 최종 수정

--- mensclub-pwa/src/api/axios.js
+++ mensclub-pwa/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // 또는 배포된 주소
+  baseURL: 'https://mensclub-backend.store', // 또는 배포된 주소
   headers: {
     'Content-Type': 'application/json',
   },
